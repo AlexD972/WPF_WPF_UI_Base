0b64b23b6b8907ef4eaaecb0617dce50d0efbb2e
DUCLOS Alexandre <alexandreduclos83@gmail.com>
2025-07-22 23:58:27 +0200

    0 : WPF_UI_Base : Initialisation du projet

---
+++ WFP_WPF_UI_Base/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WFP_WPF_UI_Base
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
